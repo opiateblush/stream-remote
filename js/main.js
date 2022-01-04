6695c7ec1f6e2c213501de33db15084c38a6e241
opiateblush <opiate.blush@mailbox.org>
2022-01-04 12:38:22 +0100

    add status bar

--- js/main.js
+++ js/main.js
@@ -9,9 +9,21 @@ import {
 } from "./constants.js";
 
 import {
+    USER_PREFERENCE_CPU_OK_KEY,
+    USER_PREFERENCE_CPU_SUFFICIENT_KEY,
+    USER_PREFERENCE_DROP_OK_KEY,
+    USER_PREFERENCE_DROP_SUFFICIENT_KEY,
+    USER_PREFERENCE_MEMORY_OK_KEY,
+    USER_PREFERENCE_MEMORY_SUFFICIENT_KEY,
+    USER_PREFERENCE_SPACE_OK_KEY,
+    USER_PREFERENCE_SPACE_SUFFICIENT_KEY
+} from "./stream-remote.js"
+
+import {
     setSocketConnectProperties,
     setSocketName,
-    clearSocketProperties
+    clearSocketProperties,
+    getUserPreferences,
 } from "./stream-remote.js"
 
 import {
@@ -19,10 +31,6 @@ import {
     removeAlert,
     showSuccessAlert
 } from "./alert.js";
-
-import {
-    createStreamRemoteSocket
-} from "./socket/socket.js"
 
 import {
     EVENT_CONNECTED, 
@@ -38,7 +46,14 @@ import {
     EVENT_STREAM_STARTED, 
     EVENT_STREAM_STARTING, 
     EVENT_STREAM_STOPPED, 
-    EVENT_STREAM_STOPPING, 
+    EVENT_STREAM_STOPPING,  
+    EVENT_CPU_USAGE, 
+    EVENT_ENCODING_DROP, 
+    EVENT_FREE_DISK_SPACE, 
+    EVENT_MEMORY_USAGE, 
+    EVENT_RENDERING_DROP, 
+    EVENT_STREAMING_DROP, 
+    EVENT_STREAM_RECORD_TIME
 } from "./socket/socket.js"
 
 import {
@@ -52,19 +67,24 @@ import {
 } from "./socket/socket.js"
 
 import {
+    createStreamRemoteSocket,
+} from "./socket/socket.js"
+
+// Necessary import even if it's not used, otherwise the "api-ready" event won't be fired.
+import {
     OVRT
 } from "./lib/ovrt-helper.js"
 
 const ID_CONTAINER_CONTINUOUS_BUTTONS = "navbar-container-buttons-continuous";
 const ID_CONTAINER_ONESHOT_BUTTONS = "navbar-container-buttons-oneshot";
 const ID_SEPARATOR_BUTTONS = "navbar-buttons-separator";
+const ID_CONTAINER_STATUS_BAR_ELEMENTS = "container-status-bar-elements";
 
 const ID_BUTTON_RECORD = "button-record";
 const ID_BUTTON_STREAM = "button-stream";
 const ID_BUTTON_REPLAY = "button-replay";
 const ID_BUTTON_REPLAY_SAVE = "button-replay-save";
 
-const ID_TEXT_STREAM_RECORD_TIME = "text-stream-record-time";
 const ID_TEXT_VERSION_AUTHOR = "text-version-author";
 
 const ID_ALERT_DISCONNECTED = "alert-disconnected";
@@ -74,10 +94,6 @@ const ID_ALERT_REPLAY_SAVE = "alert-replay-save";
 const TEXT_ALERT_REPLAY_SAVE_SUCCESS = "Replay saved!";
 const TEXT_ALERT_REPLAY_SAVE_FAILED = "Replay could not be saved!";
 const TIMEOUT_ALERT_REPLAY_SAVE = 5000;
-
-const INTERVAL_STREAM_RECORD_TIME_MS = 2000;
-
-let streamRecordTimeIntervalID;
 
 function getSocketConnectPropertiesFromSearchParams()
 {
@@ -101,34 +117,13 @@ function getSocketNameFromSearchParams()
 function onStreamRemoteSocketConnected()
 {
     updateStreamRecordButtons();
-    updateStreamRecordTimes();
 
     removeAlert(ID_ALERT_DISCONNECTED);
-
-    streamRecordTimeIntervalID = setStreamRecordTimeInterval(INTERVAL_STREAM_RECORD_TIME_MS);
 }
 
 function onStreamRemoteSocketDisconnected()
 {
-    if (streamRecordTimeIntervalID !== undefined)
-    {
-        clearInterval(streamRecordTimeIntervalID);
-        streamRecordTimeIntervalID = undefined;
-    }
-
     showErrorAlert(TEXT_ALERT_DICONNECTED, ID_ALERT_DISCONNECTED)
-}
-
-async function updateStreamRecordTimes()
-{
-    let recordTime_s = streamRemoteSocket.isSupportedRequest(REQUEST_RECORD_TIME) ? 
-        await streamRemoteSocket.request(REQUEST_RECORD_TIME) : 
-        -1;
-    let streamTime_s = streamRemoteSocket.isSupportedRequest(REQUEST_STREAM_TIME) ? 
-        await streamRemoteSocket.request(REQUEST_STREAM_TIME) :
-        -1;
-
-    setStreamRecordTime(streamTime_s, recordTime_s);
 }
 
 async function updateStreamRecordButtons()
@@ -146,13 +141,6 @@ async function updateStreamRecordButtons()
     setRecordButtonState(recordTime_s >= 0);
     setStreamButtonState(streamTime_s >= 0);
     setReplayButtonState(replayActive);
-}
-
-function setStreamRecordTimeInterval(interval_ms = 2000)
-{
-    return setInterval(() => {
-        updateStreamRecordTimes();
-    }, interval_ms);
 }
 
 function registerStreamRemoteEvents()
@@ -251,38 +239,6 @@ function setRecordButtonState(active = false, changing = false)
 function setStreamButtonState(active = false, changing = false)
 {
     setContinuousButtonState(ID_BUTTON_STREAM, active, changing);
-}
-
-function setStreamRecordTime(streamTime_s = -1, recordTime_s = -1)
-{
-    let timeElement = document.getElementById(ID_TEXT_STREAM_RECORD_TIME);
-    let totalTime_s = -1;
-
-    if (streamTime_s >= 0)
-    {
-        totalTime_s = streamTime_s;
-        timeElement.className = "fs-3 text-danger";
-    }
-    else if (recordTime_s >= 0)
-    {
-        totalTime_s = recordTime_s;
-        timeElement.className = "fs-3 text-white";
-    }
-    else
-    {
-        timeElement.className = "d-none";
-    }
-        
-    if (totalTime_s >= 0)
-    {
-        let minutes = totalTime_s / 60;
-        let hours = parseInt(minutes / 60);
-
-        minutes = parseInt(minutes - hours * 60);
-        let seconds = parseInt(totalTime_s - hours * 60 * 60 - minutes * 60);
-
-        timeElement.innerHTML = String(hours) + ":" + String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0");
-    }
 }
 
 function setupContinuousButtons()
@@ -390,6 +346,187 @@ function setupButtonsSeparator()
         separator.remove();
 }
 
+function setupStatusBar()
+{
+    let statusBar = document.getElementById(ID_CONTAINER_STATUS_BAR_ELEMENTS);
+    statusBar.innerHTML = "";
+
+    if (streamRemoteSocket.isSupportedEvent(EVENT_CPU_USAGE) || streamRemoteSocket.isSupportedEvent(EVENT_MEMORY_USAGE) ||
+        streamRemoteSocket.isSupportedEvent(EVENT_FREE_DISK_SPACE))
+    {
+        if (streamRemoteSocket.isSupportedEvent(EVENT_CPU_USAGE))
+        {
+            let cpuUsage = document.createElement("i");
+            cpuUsage.className = "bi bi-cpu-fill text-success fs-3 mx-2";
+    
+            statusBar.insertAdjacentElement("beforeend", cpuUsage);
+            
+            streamRemoteSocket.addEventListener(EVENT_CPU_USAGE, data => {
+                let userPreferences = getUserPreferences();
+    
+                cpuUsage.classList.remove("text-success");
+                cpuUsage.classList.remove("text-warning");
+                cpuUsage.classList.remove("text-danger");
+    
+                if (data <= userPreferences[USER_PREFERENCE_CPU_OK_KEY])
+                    cpuUsage.classList.add("text-success");
+                else if (data <= userPreferences[USER_PREFERENCE_CPU_SUFFICIENT_KEY])
+                    cpuUsage.classList.add("text-warning");
+                else
+                    cpuUsage.classList.add("text-danger");
+            });
+        }
+
+        if (streamRemoteSocket.isSupportedEvent(EVENT_MEMORY_USAGE))
+        {
+            let memoryUsageIndicator = document.createElement("i");
+            memoryUsageIndicator.className = "bi bi-memory text-success fs-3 mx-2";
+
+            statusBar.insertAdjacentElement("beforeend", memoryUsageIndicator);
+
+            streamRemoteSocket.addEventListener(EVENT_MEMORY_USAGE, memoryUsage_g => {
+                let userPreferences = getUserPreferences();
+    
+                memoryUsageIndicator.classList.remove("text-success");
+                memoryUsageIndicator.classList.remove("text-warning");
+                memoryUsageIndicator.classList.remove("text-danger");
+
+                if (memoryUsage_g <= userPreferences[USER_PREFERENCE_MEMORY_OK_KEY])
+                    memoryUsageIndicator.classList.add("text-success");
+                else if (memoryUsage_g <= userPreferences[USER_PREFERENCE_MEMORY_SUFFICIENT_KEY])
+                    memoryUsageIndicator.classList.add("text-warning");
+                else
+                    memoryUsageIndicator.classList.add("text-danger");
+            });
+        }
+
+        if (streamRemoteSocket.isSupportedEvent(EVENT_FREE_DISK_SPACE))
+        {
+            let freeDiskSpaceIndicator = document.createElement("i");
+            freeDiskSpaceIndicator.className = "bi bi-hdd-fill text-success fs-3 mx-2";
+
+            statusBar.insertAdjacentElement("beforeend", freeDiskSpaceIndicator);
+
+            streamRemoteSocket.addEventListener(EVENT_FREE_DISK_SPACE, freeDiskSpace_g => {
+                let userPreferences = getUserPreferences();
+    
+                freeDiskSpaceIndicator.classList.remove("text-success");
+                freeDiskSpaceIndicator.classList.remove("text-warning");
+                freeDiskSpaceIndicator.classList.remove("text-danger");
+
+                if (freeDiskSpace_g >= userPreferences[USER_PREFERENCE_SPACE_OK_KEY])
+                    freeDiskSpaceIndicator.classList.add("text-success");
+                else if (freeDiskSpace_g >= userPreferences[USER_PREFERENCE_SPACE_SUFFICIENT_KEY])
+                    freeDiskSpaceIndicator.classList.add("text-warning");
+                else
+                    freeDiskSpaceIndicator.classList.add("text-danger");
+            });
+        }
+
+        let separator = document.createElement("div");
+        separator.className = "bg-secondary rounded py-3 mx-3";
+        separator.style = "width: 0.25em;";
+
+        statusBar.insertAdjacentElement("beforeend", separator);
+    }
+
+
+    if (streamRemoteSocket.isSupportedEvent(EVENT_STREAMING_DROP) || streamRemoteSocket.isSupportedEvent(EVENT_ENCODING_DROP) ||
+        streamRemoteSocket.isSupportedEvent(EVENT_RENDERING_DROP))
+    {
+        let listener = (drop, element) => {
+            let userPreferences = getUserPreferences();
+
+            element.classList.remove("text-success");
+            element.classList.remove("text-warning");
+            element.classList.remove("text-danger");
+
+            if (drop <= userPreferences[USER_PREFERENCE_DROP_OK_KEY])
+                element.classList.add("text-success");
+            else if (drop <= userPreferences[USER_PREFERENCE_DROP_SUFFICIENT_KEY])
+                element.classList.add("text-warning");
+            else
+                element.classList.add("text-danger");
+        };
+
+        if (streamRemoteSocket.isSupportedEvent(EVENT_STREAMING_DROP))
+        {
+            let dropElement = document.createElement("i");
+            dropElement.className = "bi bi-cloud-arrow-up-fill text-success fs-3 mx-2";
+
+            statusBar.insertAdjacentElement("beforeend", dropElement);
+
+            streamRemoteSocket.addEventListener(EVENT_STREAMING_DROP, drop => listener(drop, dropElement));
+        }
+
+        if (streamRemoteSocket.isSupportedEvent(EVENT_ENCODING_DROP))
+        {
+            let dropElement = document.createElement("i");
+            dropElement.className = "bi bi-arrow-left-circle-fill text-success fs-4 mx-2";
+
+            statusBar.insertAdjacentElement("beforeend", dropElement);
+
+            streamRemoteSocket.addEventListener(EVENT_ENCODING_DROP, drop => listener(drop, dropElement));
+        }
+
+        if (streamRemoteSocket.isSupportedEvent(EVENT_RENDERING_DROP))
+        {
+            let dropElement = document.createElement("i");
+            dropElement.className = "bi bi-gpu-card text-success fs-3 mx-2";
+
+            statusBar.insertAdjacentElement("beforeend", dropElement);
+
+            streamRemoteSocket.addEventListener(EVENT_RENDERING_DROP, drop => listener(drop, dropElement));
+        }
+
+        let separator = document.createElement("div");
+        separator.className = "bg-secondary rounded py-3 mx-3";
+        separator.style = "width: 0.25em;";
+
+        statusBar.insertAdjacentElement("beforeend", separator);
+    }
+
+    let streamRecordTime = document.createElement("span");
+    streamRecordTime.className = "text-white fs-3 mx-2";
+
+    statusBar.insertAdjacentElement("beforeend", streamRecordTime);
+
+    let updateStreamRecordTime = (streamTime_s = -1, recordTime_s = -1) => {
+        let totalTime_s = 0;
+
+        streamRecordTime.classList.remove("text-white");
+        streamRecordTime.classList.remove("text-danger");
+
+        streamRecordTime.classList.add("text-white");
+    
+        if (streamTime_s >= 0)
+        {
+            totalTime_s = streamTime_s;
+            streamRecordTime.classList.add("text-danger");
+        }
+        else if (recordTime_s >= 0)
+        {
+            totalTime_s = recordTime_s;
+        }
+            
+        let minutes = totalTime_s / 60;
+        let hours = parseInt(minutes / 60);
+    
+        minutes = parseInt(minutes - hours * 60);
+        let seconds = parseInt(totalTime_s - hours * 60 * 60 - minutes * 60);
+    
+        streamRecordTime.innerHTML = String(hours) + ":" + String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0");    
+    };
+
+    streamRemoteSocket.addEventListener(EVENT_STREAM_RECORD_TIME, data => {
+        let streamTime_s = data.streamTime_s;
+        let recordTime_s = data.recordTime_s;
+        updateStreamRecordTime(data.streamTime_s, data.recordTime_s);
+    });
+
+    updateStreamRecordTime();
+}
+
 clearSocketProperties();
 
 setVersionAuthor(VERSION, AUTHOR);
@@ -408,6 +545,8 @@ createStreamRemoteSocket(socketName).then((socket) => {
     setupContinuousButtons();
     setupOneshotButtons();
     setupButtonsSeparator();
+
+    setupStatusBar();
 
     registerStreamRemoteEvents();
 
